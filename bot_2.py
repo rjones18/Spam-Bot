6fd9f55bdfa6975a966628f51b84ee14f9b9fc45
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2021-04-23 18:24:39 -0400

    Update bot_2.py

--- bot_2.py
+++ bot_2.py
@@ -1,13 +1,14 @@
 import pyautogui
 import time
 import random
+import subprocess
 
+subprocess.call("echo you have been hacked!  > texts.txt", shell=True)
 time.sleep(2)
 
-file = open('spam_texts.txt' , 'r').read()
+file = open('texts.txt' , 'r').read()
 file = file.splitlines()
 
 for _ in range(50):
 	pyautogui.typewrite(random.choice(file))
 	pyautogui.press('enter')
-	
